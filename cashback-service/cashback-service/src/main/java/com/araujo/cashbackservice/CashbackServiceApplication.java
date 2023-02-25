bf908bc22ae8006fc8943702eddce3fe87faa950
Wanderson Araujo <wanderson.araujo@portoseguro.com.br>
2023-02-25 20:37:20 -0300

    Create and configure services as monorepo

---
+++ cashback-service/cashback-service/src/main/java/com/araujo/cashbackservice/CashbackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.araujo.cashbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbackServiceApplication.class, args);
+	}
+
+}
